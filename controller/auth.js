4cf992eb7ecd5ead166502ca8e31f40114c24112
shivraj110504 <tawareshivraj11@gmail.com>
2026-01-16 16:18:39 +0530

    updated mobile number logic

--- controller/auth.js
+++ controller/auth.js
@@ -386,7 +386,7 @@ export const updateprofile = async (req, res) => {
 export const getUser = async (req, res) => {
   const { id } = req.params;
   try {
-    const userData = await user.findById(id).select("-password -phone");
+    const userData = await user.findById(id).select("-password");
     if (!userData) return res.status(404).json({ message: "User not found" });
     res.status(200).json(userData);
   } catch (error) {
